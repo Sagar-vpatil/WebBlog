d37714410cc0587b1c0732250f51c51b9a637ddf
Pratham9325 <125430290+Pratham9325@users.noreply.github.com>
2023-06-08 14:27:44 +0530

    add nwe

--- js/main.js
+++ js/main.js
@@ -26,3 +26,33 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+
+// red more //////
+const btn = document
+    .querySelector('.read-more-btn');
+
+const text = document
+    .querySelector('.card__read-more');
+
+const cardHolder = document
+    .querySelector('.card-holder');
+
+cardHolder
+    .addEventListener('click', e => {
+
+        const current = e.target;
+
+        const isReadMoreBtn = current.className.includes('read-more-btn');
+
+        if (!isReadMoreBtn)
+            return;
+
+        const currentText = e.target.parentNode.querySelector('.card__read-more');
+
+        currentText.classList.toggle('card__read-more--open');
+
+        current.textContent = current.textContent.includes('Read More...') ? 'Read Less...' : 'Read More...';
+
+    });
